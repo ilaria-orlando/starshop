a2257c8e904d2be863457fe55ca2235068637121
ilaria.orlando <ilaria.orlando1991@gmail.com>
2024-05-16 16:12:57 +0200

    add template for ship page and add links

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController
 {
-    #[Route('/starships{$id<\d+>}')]
-    public function show(int $id): Response
+    #[Route('/starships/{id<\d+>},', name: 'app_starship_show')]
+    public function show(int $id, StarshipRepository $repository): Response
     {
-            dd($id);
+        $ship = $repository->find($id);
+
+        //if id not found throw 404
+        if(!$ship){
+            throw $this-> createNotFoundException('Ship not found!');
+        }
+
+        //if id found run through template
+        return $this-> render('starship/show.html.twig', [
+            'ship' => $ship,
+        ]);
     }
 }
